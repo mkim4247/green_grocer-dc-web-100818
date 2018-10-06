77505bb334547f0ddb1598f82a874d26e0a8556d
Michael Kim <m.kim4247@gmail.com>
2018-10-06 00:15:45 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -15,27 +15,26 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-    coupons.each do |coupon_hash|
-      if (cart.keys.include?(coupon_hash[:item]))
-        if (cart[coupon_hash[:item]][:count] >= coupon_hash[:num])
-          cart[coupon_hash[:item]][:count] -= coupon_hash[:num]
-          if (cart.keys.include? ("#{coupon_hash[:item]} W/COUPON"))
-            cart["#{coupon_hash[:item]} W/COUPON"][:count] += 1 
+    coupons.each do |hash|
+      if (cart.keys.include? (hash[:item]))
+        if cart[hash[:item]][:count] >= hash[:num]
+          cart[hash[:item]][:count] -= hash[:num]
+          if cart.keys.include? ("#{hash[:item]} W/COUPON")
+            cart["#{hash[:item]} W/COUPON"][:count] += 1 
           else 
-            couponed_item = {}
-            couponed_item["#{coupon_hash[:item]} W/ COUPON"] = {:price => coupon_hash[:cost], :clearance => cart[coupon_hash[:item]][:clearance], :count => 1}
-            cart = cart.merge(couponed_item)
+            new_hash = {}
+            new_hash["#{hash[:item]} W/COUPON"] = {:price => hash[:cost], :clearance => cart[hash[:item]][:clearance], :count => 1}
+            cart = cart.merge(new_hash)
           end 
         else 
-          return cart 
+          cart 
         end 
       else 
-        return cart 
+        cart 
       end 
     end 
-    return cart
+    cart
 end
-
 
 def apply_clearance(cart)
   cart.collect do |item_name, info_hash|
